155981975143140ea5fcf23f475cb21d708e747e
wdhenarangoda <wdhenarangoda@students.nsbm.ac.lk>
2021-09-16 20:51:35 +0530

    Create Proxied URL and Deploy

--- src/context.js
+++ src/context.js
@@ -26,7 +26,7 @@ export class Provider extends Component {
   componentDidMount() {
     axios
       .get(
-        `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`
+        `https://arcane-springs-59786.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`
       )
       .then((res) => {
         console.log(res.data);
